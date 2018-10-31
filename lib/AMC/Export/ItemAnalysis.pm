b00a153aa144dc47386b5e005b0e3418e9de04a7
Matthew Leingang <leingang@nyu.edu>
2018-10-31 17:04:41 -0400

    Add scores and responses to main data structure
    Now `export` actually does something! Next would be to add the item
    analysis.

--- lib/AMC/Export/ItemAnalysis.pm
+++ lib/AMC/Export/ItemAnalysis.pm
@@ -2,6 +2,7 @@
 package AMC::DataModule::capture::ItemAnalysis;
 
 use parent "AMC::DataModule::capture";
+use Data::Dumper;# for debugging
 
 # convert number to letter
 # stolen from AMC::Export::CSV
@@ -31,12 +32,19 @@ sub i_to_a {
 # or maybe string of 0/1 separated by ;?  
 # might depend on FR/MC 
 sub question_response {
+    # print "question_response: BEGIN\n";
     my ($self,$student,$copy,$question)=@_;
+    # print "question_response: \$student:", $student, "\n";
+    # print "question_response: \$copy:", $copy, "\n";
+    # print "question_response: \$question:", Dumper($question), "\n";
     my $dt = $self->{'darkness_threshold'};
     my $dtu = $self->{'darkness_threshold_up'};
+    # print "question_response: \$dt:", $dt, "\n";
+    # print "question_response: \$dtu:", $dtu, "\n";
     my $t='';
-    my @tl=$self->ticked_list($student,$copy,$question->{'question'},$dt,$dtu);
-    if($self->has_answer_zero(@$student,$copy,$question->{'question'})) {
+    my @tl=$self->ticked_list($student,$copy,$question,$dt,$dtu);
+    print "question_response: \@tl:", Dumper(\@tl), "\n";
+    if($self->has_answer_zero(@$student,$copy,$question)) {
         if(shift @tl) {
             $t.='0';
         }
@@ -44,6 +52,7 @@ sub question_response {
     for my $i (0..$#tl) {
         $t.=$self->i_to_a($i+1) if($tl[$i]);
     }
+    # print "question_response: END. \$t=", $t, "\n";
     return $t;
 }
 
@@ -60,6 +69,7 @@ use Encode;
 use Data::Dumper;# for debugging
 
 sub new {
+    print "new: BEGIN\n";
     my $class = shift;
     my $self  = $class->SUPER::new();
     $self->{'out.nom'}="";
@@ -76,12 +86,16 @@ sub new {
 
 sub load {
     my ($self)=@_;
+    print "load: BEGIN", "\n";
     $self->SUPER::load();
     $self->{'_capture'}=$self->{'_data'}->module('capture');
     bless $self->{'_capture'}, 'AMC::DataModule::capture::ItemAnalysis';
+    print "load: about to save variables...";
     for my $var ('darkness_threshold','darkness_threshold_up') {
-        $self->{'_capture'}->{$var} = $self->{'_scoring'}->variable($var);
+        $self->{'_capture'}->{$var} = $self->{'_scoring'}->variable_transaction($var);
     }
+    print "done\n";
+    print "load: END\n";
 }
 
 # format a number
@@ -134,6 +148,7 @@ sub parse_string {
 # $out->{'items'}->[$i]->{'histogram'}->{$k}->{'weight'}
 sub get_data {
     my ($self) = @_;
+    print "get_data:BEGIN\n";
     $o = {};
     $o->{'items'} = [];
     $o->{'responses'} = [];
@@ -159,7 +174,7 @@ sub get_data {
     # - @codes is student identifying codes,
     # - @questions is exam questions.
     # - last argument is "plain" (no ticks) or not (yes ticks).
-    $self->codes_questions(\@codes,\@questions,0);
+    $self->codes_questions(\@codes,\@questions,1);
 
     # begin loop on each student record
     for my $m (@$marks) {
@@ -183,9 +198,11 @@ sub get_data {
 
 }
 
-sub export {
+
+sub export {    
     my ($self,$fichier)=@_;
     my $sep=$self->{'out.separateur'};
+    print "export: BEGIN\n";
 
     open(OUT,">:encoding(".$self->{'out.encodage'}.")",$fichier);
 
@@ -197,6 +214,89 @@ sub export {
     print OUT Dumper($data);
     
     close(OUT);
+}
+
+# overloading just to track a transaction error
+sub pre_process {
+    print "pre_process: BEGIN\n";
+    my ($self)=@_;
+
+    $self->{'sort.keys'}=$sorting{lc($1)}
+      if($self->{'sort.keys'} =~ /^\s*([lmin])/i);
+    $self->{'sort.keys'}=[] if(!$self->{'sort.keys'});
+
+    $self->load();
+
+    print "pre_process: EXPP\n";
+    $self->{'_scoring'}->begin_read_transaction('EXPP');
+
+    my $lk=$self->{'_assoc'}->variable('key_in_list');
+    my %keys=();
+    my @marks=();
+    my @post_correct=$self->{'_scoring'}->postcorrect_sc;
+
+    # Get all students from the marks table
+
+    my $sth=$self->{'_scoring'}->statement('marks');
+    $sth->execute;
+  STUDENT: while(my $m=$sth->fetchrow_hashref) {
+      next STUDENT if((!$self->{'noms.postcorrect'}) &&
+		      $m->{student}==$post_correct[0] &&
+		      $m->{'copy'}==$post_correct[1]);
+
+      $m->{'abs'}=0;
+      $m->{'student.copy'}=studentids_string($m->{'student'},$m->{'copy'});
+
+      # Association key for this sheet
+      $m->{'student.key'}=$self->{'_assoc'}->get_real($m->{'student'},$m->{'copy'});
+      $keys{$m->{'student.key'}}=1;
+
+      # find the corresponding name
+      my ($n)=$self->{'noms'}->data($lk,$m->{'student.key'},test_numeric=>1);
+      if($n) {
+	$m->{'student.name'}=$n->{'_ID_'};
+	$m->{'student.line'}=$n->{'_LINE_'};
+	$m->{'student.all'}={%$n};
+        # $n->{$lk} should be equal to $m->{'student.key'}, but in
+        # some cases (older versions), the code stored in the database
+        # has leading zeroes removed...
+        $keys{$n->{$lk}}=1;
+      } else {
+	for(qw/name line/) {
+	  $m->{"student.$_"}='?';
+	}
+      }
+      push @marks,$m;
+    }
+
+    # Now, add students with no mark (if requested)
+
+    if($self->{'noms.useall'}) {
+      for my $i ($self->{'noms'}->liste($lk)) {
+	if(!$keys{$i}) {
+	  my ($name)=$self->{'noms'}->data($lk,$i,test_numeric=>1);
+	  push @marks,
+	    {'student'=>'',
+	     'copy'=>'',
+	     'student.copy'=>'',
+	     'abs'=>1,
+	     'student.key'=>$name->{$lk},
+	     'mark'=>$self->{'noms.abs'},
+	     'student.name'=>$name->{'_ID_'},
+	     'student.line'=>$name->{'_LINE_'},
+	     'student.all'=>{%$name},
+	    };
+	}
+      }
+    }
+
+    # sorting as requested
+
+    debug "Sorting with keys ".join(", ",@{$self->{'sort.keys'}});
+    $self->{'marks'}=[sort { $self->compare($a,$b); } @marks];
+
+    $self->{'_scoring'}->end_transaction('EXPP');
+    print "pre_process: END\n";
 }
 
 1;
