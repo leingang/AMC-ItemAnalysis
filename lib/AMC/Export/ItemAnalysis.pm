2f8473d700439283a7495ad838e21839053552f5
Matthew Leingang <leingang@nyu.edu>
2018-11-01 06:22:16 -0400

    Migrate extension code into separate module
    In order to better understand the implementation of `export` from the
    `AMC::Export::CSV` package, I split off some code which was only using
    methods from `AMC::DataModule::capture`.  I put this code into a
    separate `AMC::ItemAnalysis::capture` package that extends the original
    `AMC::DataModule::capture`.
    
    Hopefully this will make for better decoupling of packages, and more
    readable code.

--- lib/AMC/Export/ItemAnalysis.pm
+++ lib/AMC/Export/ItemAnalysis.pm
@@ -1,66 +1,9 @@
-# additional methods to this package
-package AMC::DataModule::capture::ItemAnalysis;
 
-use parent "AMC::DataModule::capture";
-use Data::Dumper;# for debugging
-
-# convert number to letter
-# stolen from AMC::Export::CSV
-sub i_to_a {
-  my ($self,$i)=@_;
-  if($i==0) {
-    return('0');
-  } else {
-    my $s='';
-    while($i>0) {
-      $s = chr(ord('a')+(($i-1) % 26)) . $s;
-      $i = int(($i-1)/26);
-    }
-    $s =~ s/^([a-z])/uc($1)/e;
-    return($s);
-  }
-}
-
-
-# The response from a student on a question
-#
-# $student : student number
-# $copy : copy number
-# $question : question hashref
-# 
-# returns: string of concatenated responses by letter
-# or maybe string of 0/1 separated by ;?  
-# might depend on FR/MC 
-sub question_response {
-    # print "question_response: BEGIN\n";
-    my ($self,$student,$copy,$question)=@_;
-    # print "question_response: \$student:", $student, "\n";
-    # print "question_response: \$copy:", $copy, "\n";
-    # print "question_response: \$question:", Dumper($question), "\n";
-    my $dt = $self->{'darkness_threshold'};
-    my $dtu = $self->{'darkness_threshold_up'};
-    # print "question_response: \$dt:", $dt, "\n";
-    # print "question_response: \$dtu:", $dtu, "\n";
-    my $t='';
-    my @tl=$self->ticked_list($student,$copy,$question,$dt,$dtu);
-    print "question_response: \@tl:", Dumper(\@tl), "\n";
-    if($self->has_answer_zero(@$student,$copy,$question)) {
-        if(shift @tl) {
-            $t.='0';
-        }
-    }
-    for my $i (0..$#tl) {
-        $t.=$self->i_to_a($i+1) if($tl[$i]);
-    }
-    # print "question_response: END. \$t=", $t, "\n";
-    return $t;
-}
-
-# main package
 package AMC::Export::ItemAnalysis;
 
 use AMC::Basic;
 use AMC::Export;
+use AMC::ItemAnalysis::capture;
 
 use Encode;
 
@@ -89,7 +32,7 @@ sub load {
     print "load: BEGIN", "\n";
     $self->SUPER::load();
     $self->{'_capture'}=$self->{'_data'}->module('capture');
-    bless $self->{'_capture'}, 'AMC::DataModule::capture::ItemAnalysis';
+    bless $self->{'_capture'}, 'AMC::ItemAnalysis::capture';
     print "load: about to save variables...";
     for my $var ('darkness_threshold','darkness_threshold_up') {
         $self->{'_capture'}->{$var} = $self->{'_scoring'}->variable_transaction($var);
