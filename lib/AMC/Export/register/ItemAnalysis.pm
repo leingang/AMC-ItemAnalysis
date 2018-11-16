4cfddbd62276dfd89837f388de7ee9fb443ee58a
Matthew Leingang <leingang@nyu.edu>
2018-11-16 18:45:19 -0500

    Add GPLv3 license information to source files and README (Fixes #13)

--- lib/AMC/Export/register/ItemAnalysis.pm
+++ lib/AMC/Export/register/ItemAnalysis.pm
@@ -1,21 +1,20 @@
 #
-# Copyright (C) 2012 Alexis Bienvenue <paamc@passoire.fr>
+# Copyright (C) 2018 Matthew Leingang <leingang@nyu.edu>
 #
-# This file is part of Auto-Multiple-Choice
+# This file is part of AMC-ItemAnalysis
 #
-# Auto-Multiple-Choice is free software: you can redistribute it
-# and/or modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation, either version 2 of
-# the License, or (at your option) any later version.
+# AMC-ItemAnalysis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 #
-# Auto-Multiple-Choice is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# AMC-ItemAnalysis is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with Auto-Multiple-Choice.  If not, see
-# <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along
+# with AMC-ItemAnalysis.  If not, see <https://www.gnu.org/licenses/>.
 
 package AMC::Export::register::ItemAnalysis;
 
--- t/20-analyze-mrit-mt1.t
+++ t/20-analyze-mrit-mt1.t
@@ -19,7 +19,23 @@ with values computed by another method (C<pandas>).
 L<https://github.com/leingang/AMC-ItemAnalysis/issues/11>
 
 =cut
-
+#
+# Copyright (C) 2018 Matthew Leingang <leingang@nyu.edu>
+#
+# This file is part of AMC-ItemAnalysis
+#
+# AMC-ItemAnalysis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# AMC-ItemAnalysis is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with AMC-ItemAnalysis.  If not, see <https://www.gnu.org/licenses/>.
 use 5.006;
 use strict;
 use warnings;
