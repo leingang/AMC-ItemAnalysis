752ff8f04131b6bbe9ab5cfe52e8d52021d0d0cd
Matthew Leingang <leingang@nyu.edu>
2018-11-16 18:52:28 -0500

    Bump version to v0.1

--- lib/AMC/ItemAnalysis.pm
+++ lib/AMC/ItemAnalysis.pm
@@ -33,7 +33,7 @@ use warnings;
 # David Golden's recommendations for version numbers <http://bit.ly/1g8EbKi> 
 # are used, e.g. v0.1.2 is "0.001002" and v1.2.3dev4 is "1.002002_004".
 
-our $VERSION = '0.000000_001';
+our $VERSION = '0.001000_001';
 $VERSION = eval $VERSION;  # runtime conversion to numeric value
 
 
