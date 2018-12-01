84158942228dc098d014793e61a733e324866749
Matthew Leingang <leingang@nyu.edu>
2018-11-30 19:47:19 -0500

    Add POD and privatize private method

--- lib/AMC/ItemAnalysis/capture.pm
+++ lib/AMC/ItemAnalysis/capture.pm
@@ -1,36 +1,42 @@
-# additional methods to this package
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+AMC::ItemAnalysis::capture - Add methods to AMC::DataModule::capture
+
+=cut
+
 package AMC::ItemAnalysis::capture;
 
 use parent q(AMC::DataModule::capture);
 use Data::Dumper;# for debugging
 
-# convert number to letter
-# stolen from AMC::Export::CSV
-sub i_to_a {
-  my ($self,$i)=@_;
-  if($i==0) {
-    return('0');
-  } else {
-    my $s='';
-    while($i>0) {
-      $s = chr(ord('a')+(($i-1) % 26)) . $s;
-      $i = int(($i-1)/26);
-    }
-    $s =~ s/^([a-z])/uc($1)/e;
-    return($s);
-  }
-}
+=head1 METHOD
 
+=head2 question_response
 
-# The response from a student on a question
-#
-# $student : student number
-# $copy : copy number
-# $question : question hashref
-# 
-# returns: string of concatenated responses by letter
-# or maybe string of 0/1 separated by ;?  
-# might depend on FR/MC 
+C<< $obj->question_response($student,$copy,$question) >> looks up
+the response from a student on a question.
+
+Parameters:
+
+=over
+
+=item C<$student>: student number
+
+=item C<$copy>: copy number
+
+=item C<$question>: question hashref
+
+=back
+
+Returns: a string of concatenated responses by letter.  If a student marks
+answers 1 and 3, 'AC' will be the returned.
+
+=cut
+
 sub question_response {
     # print "question_response: BEGIN\n";
     my ($self,$student,$copy,$question)=@_;
@@ -50,10 +56,28 @@ sub question_response {
         }
     }
     for my $i (0..$#tl) {
-        $t.=$self->i_to_a($i+1) if($tl[$i]);
+        $t .= $self->_i_to_a($i+1) if($tl[$i]);
     }
     # print "question_response: END. \$t=", $t, "\n";
     return $t;
+}
+
+
+# convert number to letter
+# stolen from AMC::Export::CSV
+sub _i_to_a {
+  my ($self,$i)=@_;
+  if($i==0) {
+    return('0');
+  } else {
+    my $s='';
+    while($i>0) {
+      $s = chr(ord('a')+(($i-1) % 26)) . $s;
+      $i = int(($i-1)/26);
+    }
+    $s =~ s/^([a-z])/uc($1)/e;
+    return($s);
+  }
 }
 
 1;
