54a9d4acf60f9fa9eb0b4411ff194e4bd5ada4cc
Matthew Leingang <leingang@nyu.edu>
2019-01-01 15:12:38 -0500

    Add a `plugin` action to `Build` script
    Now `Build plugin` builds the plugin tarball.
    
    Closes #12.

--- lib/AMC/Plugin/Build.pm
+++ lib/AMC/Plugin/Build.pm
@@ -1,6 +1,5 @@
-
 # Copyright (C) 2018-19 Matthew Leingang <leingang@nyu.edu>
-#
+# 
 # This file is part of AMC-ItemAnalysis
 #
 # AMC-ItemAnalysis is free software: you can redistribute it and/or modify
@@ -46,13 +45,73 @@ The compressed tarball can now be imported into AMC.
 
 =head1 SUBROUTINES/METHODS
 
-=head2 plugin
+=head2 ACTION_plugin
 
-Create a tarball that can be installed as an AMC.  
+Create a tarball that can be installed as an AMC plugin.
 See L<https://github.com/leingang/AMC-ItemAnalysis/issues/12>.
 
 Based on the C<ACTION_ppmdist> subroutine from
 L<Module::Build::Base>.
+
+=cut
+
+sub ACTION_plugin {
+    my $self = shift;
+    my $plugin_name = $self->plugin_name;
+    my $plugin_dir = $self->plugin_dir;
+    my $plugin_tarball = $plugin_dir . ".tar.gz";
+    $self->delete_filetree($plugin_dir);
+    $self->log_info( "Creating $plugin_dir\n" );
+    $self->add_to_cleanup( $plugin_dir, $plugin_tarball );
+ 
+    my %types = ( # translate types/dirs to those expected by ppm
+    lib     => 'perl',
+    # arch    => 'arch',
+    # bin     => 'bin',
+    # script  => 'script',
+    bindoc  => 'man/man1',
+    libdoc  => 'man/man3',
+    binhtml => undef,
+    libhtml => undef,
+    );
+ 
+    foreach my $type ($self->install_types) {
+        next if exists( $types{$type} ) && !defined( $types{$type} );
+ 
+        my $dir = File::Spec->catdir( $self->blib, $type );
+        next unless -e $dir;
+ 
+        my $files = $self->rscan_dir( $dir );
+        foreach my $file ( @$files ) {
+        next unless -f $file;
+        my $rel_file =
+            File::Spec->abs2rel( File::Spec->rel2abs( $file ),
+                                File::Spec->rel2abs( $dir  ) );
+        my $to_file  =
+            File::Spec->catfile( $plugin_dir,
+                                exists( $types{$type} ) ? $types{$type} : $type,
+                                $rel_file );
+        $self->copy_if_modified( from => $file, to => $to_file );
+        }
+    }
+    
+    # foreach my $type ( qw(bin lib) ) {
+    #     $self->htmlify_pods( $type, File::Spec->catdir($ppm, 'blib', 'html') );
+    # }
+ 
+    $self->make_tarball( $plugin_dir, $plugin_name);
+
+    $self->delete_filetree( $plugin_dir );    
+}
+
+=head2 plugin_name
+
+The name of the plugin.
+
+It can be specified at construction by the C<plugin_name> keyword argument.
+
+If not earlier specified, this method split the module name by C<::> and
+returns the last part.  
 
 =cut
 
@@ -68,23 +127,22 @@ sub plugin_name {
     }
 }
 
-sub ACTION_plugin {
-    my $self = shift;
-    my $plugin_name = $self->plugin_name;
-<<<<<<< Updated upstream
+=head2 plugin_dir
 
-=======
->>>>>>> Stashed changes
+The directory the plugin tarball unzips to.  Defaults to the the plugin name.
+
+=cut
+
+sub plugin_dir {
+    my $self = shift;
+    return $self->plugin_name;
 }
+
 
 =head1 AUTHOR
 
 Matthew Leingang, C<< <leingang@nyu.edu> >>
 
-
-=head1 SUPPORT
-
-Look at the submodules.
 
 =head1 LICENSE AND COPYRIGHT
 
--- xt/build-plugin.t
+++ xt/build-plugin.t
@@ -1,4 +1,21 @@
-#perl -T
+#!/usr/bin/env perl
+#
+# Copyright (C) 2018-19 Matthew Leingang <leingang@nyu.edu>
+#
+# This file is part of AMC-ItemAnalysis
+#
+# AMC-ItemAnalysis is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# AMC-ItemAnalysis is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with AMC-ItemAnalysis.  If not, see <https://www.gnu.org/licenses/>.
 =head1
 
 build-plugin.t - test the AMC::Plugin::Build module
@@ -22,14 +39,19 @@ is ($build_foo->plugin_name,'foo','plugin_name override works');
 my $build = AMC::Plugin::Build->new( module_name =>'AMC::ItemAnalysis' );
 is ($build->plugin_name,'ItemAnalysis','plugin_name defaults correctly');
 
-my $temp_dir = File::Temp->newdir();
-my $tarball_path = File::Spec->catdir($temp_dir,$build->plugin_name . ".tar.gz");
+# my $temp_dir = File::Temp->newdir(CLEANUP=>0);
+my $tarball_path = File::Spec->catdir($build->plugin_name . ".tar.gz");
 
 $build->dispatch("build");
 # No return value for this action, so can't use is() 
-# TODO: search $temp_dir for the plugin tarball
-# File::Find does more than search, though.
-ok($build->dispatch("plugin",destdir=>$temp_dir));
+# ok($build->_do_in_dir($temp_dir,
+#     sub {
+#         $build->dispatch("plugin")
+#             or die "Error building plugin in in directory '$temp_dir': $!";
+#     }
+# ));
+
+$build->dispatch("plugin") or die "Error building plugin: $!";
 is(-f $tarball_path,1,"Tarball exists: $tarball_path");
 
 
